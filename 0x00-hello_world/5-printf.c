7388dfa9759cfdae82f46be343ee019ea9c06183
Asamkpere Kelvin <asamkperekelvin@gmail.com>
2023-02-17 12:02:01 -0800

    Printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printf function
+ *
+ * return: always 0 (success)
+ */
+{
+	printf("with proper grammer, but with the outcome is a piece of art, \n");
+	return (0);
+}
