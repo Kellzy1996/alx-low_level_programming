f0c153abe22164200ebcc385d7862c54448f74d5
Asamkpere Kelvin <asamkperekelvin@gmail.com>
2023-02-17 12:30:55 -0800

    Printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  * return: always 0 (success)
  */
 {
-	printf("with proper grammer,but with the outcome is a piece of art,\n");
+	printf("with proper grammer, but with the outcome is a piece of art,\n");
 	return (0);
 }
